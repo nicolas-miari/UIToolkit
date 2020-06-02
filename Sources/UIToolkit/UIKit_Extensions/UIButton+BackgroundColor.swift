12fc3db87a03da297521b4e530cc5086da8d981f
Nicolás Miari <nicolasmiari@gmail.com>
2020-06-02 20:32:49 +0900

    Fix button extension

--- Sources/UIToolkit/UIKit_Extensions/UIButton+BackgroundColor.swift
+++ Sources/UIToolkit/UIKit_Extensions/UIButton+BackgroundColor.swift
@@ -7,3 +7,36 @@
 //
 
 import UIKit
+
+public extension UIButton {
+
+    // MARK: - Per-State Background Color
+
+    /**
+     Adds support for specifying a custom background color for each control
+     state.
+
+     - note: Internally, it just generates a **solid image** of the specified color and from then on it
+     delegates to `setBackgroundImage(_:for:)` (extensions cannot declare stored instance properties, so
+     it would be impossible to "store" the spearate background colors for each state). Because of this,
+     the implementation depends on a separate extension to the `UIImage` class that defines the convenience
+     initializer `init(color:size:)`.
+     */
+    @objc dynamic func setBackgroundColor(_ color: UIColor?, for state: UIControl.State) {
+        if let color = color {
+            self.setBackgroundImage(UIImage(color: color), for: state)
+        } else {
+            self.setBackgroundImage(nil, for: state)
+        }
+    }
+
+    ///
+    @objc dynamic var titleLabelFont: UIFont? {
+        get {
+            return titleLabel?.font
+        }
+        set {
+            titleLabel?.font = newValue
+        }
+    }
+}
